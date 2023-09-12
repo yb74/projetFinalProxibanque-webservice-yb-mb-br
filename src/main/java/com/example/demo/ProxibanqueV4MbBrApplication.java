8fe9410b6300a098fe61ce4020c4bc2541a1a4d4
Younes Bouaziz <yb74@hotmail.fr>
2023-09-12 19:07:15 +0200

    first commit

---
+++ src/main/java/com/example/demo/ProxibanqueV4MbBrApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxibanqueV4MbBrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxibanqueV4MbBrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/ProxibanqueV4MbBrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxibanqueV4MbBrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
